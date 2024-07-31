2531702a2af2c38facba5a8b247bde0a5cc6227f
Manuel Loaiza Vasquez <19366788+ManuelLoaizaV@users.noreply.github.com>
2024-07-31 15:03:20 +0000

    Enhance comments

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // I'm feeling confident, so I will complete the challenges.
 const pronouns = ["the", "our"];
 const adjectives = ["great", "big", "last"];
-// The ending of my last two words intentionally are top level domains intentionally.
+// The ending of my last two words intentionally are top level domains.
 const nouns = ["jogger", "racoon", "ofus", "agario"];
 
 // First challenge: Adding several types of extensions.
@@ -13,6 +13,7 @@ for (let i = 0; i < pronouns.length; i++) {
             for (let l = 0; l < topLevelDomains.length; l++) {
                 let domainName = pronouns[i] + adjectives[j];
                 // Second challenge: if the noun ends with a top level domain, do magic :D
+                // TODO: Discover if there is simpler way to obtain a prefix specifying number of characters from the end I won't take.
                 if (nouns[k].endsWith(topLevelDomains[l])) {
                     domainName += nouns[k].substring(0, nouns[k].length - topLevelDomains[l].length);
                 } else {
